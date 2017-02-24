c220b1f2784928b9d8a7300e72b70fa6ada2ef7d
upjoes <joe.savage@theuniprogroup.com>
2017-02-24 15:03:04 +0000

    restore copycat to original

--- copycat.js
+++ copycat.js
@@ -5,73 +5,73 @@ const apiai = require('apiai')(process.env.API_CLIENT_TOKEN || COPYCAT_DEMO_CLIE
 const md5 = require('md5');
 
 function copycat(express) {
-  app.launch((req, res) => {
+    app.launch((req, res) => {
+        res.shouldEndSession(false);
+    res.reprompt(`Are you still there? I'll repeat what you say.`);
+    res.say(`Hello, please say something and I'll repeat it back to you.`);
+});
+
+    app.intent('catchAllIntent', {
+            slots: {
+                catch: 'CatchAll',
+            },
+            utterances: [
+                '{-|catch}',
+            ]
+        },
+        (req, res) => {
+        const catchall = req.slot('catch');
+    res.say(catchall);
     res.shouldEndSession(false);
-    res.reprompt('Are you still there? I\'ll repeat what you say.');
-    res.say('Hello, please say something and I\'ll repeat it back to you.');
-  });
+    res.reprompt('Still there?');
+    console.log('got:', catchall);
 
-  app.intent('catchAllIntent', {
-      slots: {
-        catch: 'CatchAll',
-      },
-      utterances: [
-        '{-|catch}',
-      ]
-    },
-    (req, res) => {
-      const catchall = req.slot('catch');
-      res.say(catchall);
-      res.shouldEndSession(false);
-      res.reprompt('Still there?');
-      console.log('got:', catchall);
-
-      apiaiQuery(catchall, req.sessionDetails.sessionId, function (error, response) {
+    apiaiQuery(catchall, req.sessionDetails.sessionId, function (error, response) {
         if (error) {
-          console.error('API.ai error', error);
+            console.error('API.ai error', error);
         } else {
-          console.log('API response:', response);
+            console.log('API response:', response);
         }
-      });
-    }
-  );
+    });
+}
+);
 
-  app.intent('errorIntent', (req, res) => {
-    res.say('There was an error!');
-  });
+    app.intent('errorIntent', (req, res) => {
+        res.say('There was an error!');
+});
 
-  app.error = (e, req, res) => {
-    res.say('Got exception. ' + e.message);
-    console.error('error handler! ', e);
-  };
+    app.error = (e, req, res) => {
+        res.say('Got exception. ' + e.message);
+        console.error('error handler! ', e);
+    };
 
-  app.sessionEnded((req, res) => {
-    console.log('sessionEnded because:', req.data.request.reason);
-  });
+    app.sessionEnded((req, res) => {
+        console.log('sessionEnded because:', req.data.request.reason);
+});
 
-  app.express(express, '/alexa/', true);
+    app.express(express, '/alexa/', true);
 }
 
 function apiaiQuery(query, sessionId, cb) {
-  if (apiai) {
-    const request = apiai.textRequest(query, {
-      // Apply md5 the session id to fit within API.ai's 36 character limit
-      sessionId: md5(sessionId)
-    });
+    if (apiai) {
+        const request = apiai.textRequest(query, {
+            // Apply md5 the session id to fit within API.ai's 36 character limit
+            sessionId: md5(sessionId)
+        });
 
-    request.on('response', function (response) {
-      cb(null, response);
-    });
+        request.on('response', function (response) {
+            cb(null, response);
+        });
 
-    request.on('error', function (error) {
-      console.log(error);
-      cb(error);
-    });
+        request.on('error', function (error) {
+            console.log(error);
+            cb(error);
+        });
 
-    request.end();
-  }
+        request.end();
+    }
 }
 
 module.exports = {
-  copycat: copycat
+    copycat: copycat
 };
